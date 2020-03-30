2421adad1f93a14bab4b7c7829d229223cc66469
hbenbow <harriet.benbow@gmail.com>
2020-03-30 16:50:33 +0100

    Second push of qtls scripts

--- QTLs.R
+++ QTLs.R
@@ -1,3 +1,43 @@
+setwd("~/Documents/Hotspots/Paper_version_4/Markers/Meta_qtl_maps/")
+chroms<-dir(pattern="map")
+chromosome<-substr(chroms, 1, 2)
+
+maps<-list()
+qtl_list<-list()
+all<-list()
+for(i in chromosome){
+  map<-read.delim(file=paste(i, "_Map.txt.map.txt", sep=""), header=F)
+  colnames(map)<-c("order", "Marker", "cM")
+  map$Chromosome<-paste(i)
+  maps[[length(maps)+1]] = map
+  qtl<-read.delim(paste(i, "_QTL.txt.qtl", sep=""), header=F)
+  colnames(qtl)<-c("QTL", "Resistance", "Ontology", "Place", "Year", "Chromosome" ,"Linkage_group", "LOD", "r2", "Peak", "cM_Start", "cM_end")
+  qtl_list[[length(qtl_list)+1]]= qtl
+  qtls<-unique(qtl$QTL)
+  List<-list()
+  for(q in qtls){
+    data<-qtl[(qtl$QTL==q),]
+    min<-data$cM_Start
+    max<-data$cM_end
+    qtl_position<-map[!(map$cM<=min),]
+    qtl_position<-qtl_position[!(qtl_position$cM>=max),]
+    qtl_position$QTL<-paste(q)
+    qtl_position$Chromosome<-paste(i)
+    List[[length(List)+1]]=qtl_position
+  }
+  df<-do.call(rbind.data.frame, List)
+  all[[length(all)+1]]<-df
+}
+maps<-do.call(rbind.data.frame, maps)
+maps$Chromosome<-paste("chr", maps$Chromosome, sep="")
+all<-do.call(rbind.data.frame, all)
+all$Chromosome<-paste("chr", all$Chromosome, sep="")
+qtls<-do.call(rbind.data.frame, qtl_list)
+
+
+dir.create("~/Documents/Hotspots/Paper_version_4/QTLs")
+setwd("~/Documents/Hotspots/Paper_version_4/QTLs")
+
 qtls_list<-list()
 for(i in 1:nrow(qtls)){
   data<-qtls[i,]
