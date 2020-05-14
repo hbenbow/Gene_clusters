e6b3c19a70a88d20f9e26bad4464ebd4a1888fd7
hbenbow <harriet.benbow@gmail.com>
2020-05-14 10:23:26 +0100

    added agregation of permutations and percentage change graphs

--- Permutations.R
+++ Permutations.R
@@ -3,20 +3,21 @@ require(zoo)
 library(plyr)
 library(dplyr)
 library(tidyr)
+options(scipen = 999)
 
 # Read in gene coordinate file, containing Chromosome name, gene start posiiton, gene end
 # position, gene id, and strand. Do not need strand.
 
-all <- read.csv("../wheat_all.csv", header=T)
+all <- read.csv("~/Documents/Hotspots/Paper_version_4/wheat_all.csv", header=T)
 colnames(all)<-c( "Chromosome", "start", "end", "GeneID", "Score", "strand")
 # Read in expression scores. This is a .csv file in which each row is a gene
 # an d order must correspond to the order in the gene coordinate file. Each column after 
 # the gene column is 
-expression_scores<-read.csv("../expression_scores.csv", header=T)
+expression_scores<-read.csv("~/Documents/Hotspots/Paper_version_4/expression_scores.csv", header=T)
 
 
 diseases<-colnames(expression_scores)[-1]
-permutations=1000
+permutations=10
 dir.create("Permutations")
 # this section reads in all files and does the window analysis
 # test
@@ -69,8 +70,35 @@ for (d in diseases){
   
 }
 
-conseqs<-do.call(rbind.data.frame, conseq2)
 densitys<-do.call(rbind.data.frame, Density)
+all<-aggregate(densitys$n, by=list(densitys$density, densitys$Stress), FUN=sum)
+all$percentage<-NULL
+colnames(all)<-c("density", "Stress", "n")
+windows<-aggregate(all$n, by=list(all$Stress), FUN=sum)
+colnames(windows)<-c("Stress", "total")
+percent<-merge(all, windows, by="Stress")
+percent$percentage<-percent$n/percent$total*100
+percent<-percent[(percent$density > 0),]
+ggplot(percent, aes(x=as.factor(density), y=percentage)) + geom_point(size=2) + 
+  facet_wrap(~Stress) + theme_bw() + scale_y_log10() + geom_hline(yintercept=0.01) +
+  xlab("Density") + ylab ("Percentage chance") +
+  theme(text = element_text(size=15)) 
+
+conseqs<-do.call(rbind.data.frame, conseq2)
+all<-aggregate(conseqs$n, by=list(conseqs$Consecutive, conseqs$Stress), FUN=sum)
+all$percentage<-NULL
+colnames(all)<-c("consecutive", "Stress", "n")
+windows<-aggregate(all$n, by=list(all$Stress), FUN=sum)
+colnames(windows)<-c("Stress", "total")
+percent<-merge(all, windows, by="Stress")
+percent$percentage<-percent$n/percent$total*100
+percent<-percent[(percent$consecutive > 0),]
+ggplot(percent, aes(x=as.factor(consecutive), y=percentage)) + geom_point(size=2) + 
+  facet_wrap(~Stress) + theme_bw() + scale_y_log10() + geom_hline(yintercept=0.01) +
+  xlab("Density") + ylab ("Percentage chance") +
+  theme(text = element_text(size=15)) 
+
+
 write.csv(conseqs, file="Permutations/consec.csv", row.names=F)
 write.csv(densitys, file="Permutations/density.csv", row.names=F)
 
