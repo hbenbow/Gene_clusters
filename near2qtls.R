f7dd9292342ec350e74e003b906a2da630c65237
hbenbow <harriet.benbow@gmail.com>
2020-05-19 15:34:57 +0100

    added summary stats;

--- near2qtls.R
+++ near2qtls.R
@@ -7,6 +7,11 @@ final_hotspots <- read.csv("~/Documents/Hotspots/Paper_version_4/final_hotspots.
 QTL_database <- read.csv("~/Documents/Hotspots/Paper_version_4/QTLs/QTL_database.csv")
 QTL_database$Chromosomes<-paste("chr", QTL_database$Chromosomes, sep="")
 
+number<-unique(subset
+  (all_markers_positions, 
+    all_markers_positions$Feature %in% QTL_database$Linked.markers))
+table(number$Platform)
+
 all_qtls<-list()
 qtls<-QTL_database$General.number
 for(qtl in qtls){
@@ -15,17 +20,17 @@ for(qtl in qtls){
   chr<-data$Chromosomes
   position<-subset(all_markers_positions, all_markers_positions$Chromosome %in% chr)
   position<-subset(position, position$Feature %in% marker)
-   clusters<-subset(final_hotspots, final_hotspots$Chromosome %in% chr)
+  clusters<-subset(final_hotspots, final_hotspots$Chromosome %in% chr)
   len<-nrow(clusters)
   lenp<-nrow(position)
   if(lenp>0 && len>0){
-      clusters$qtl<-paste(qtl)
-      clusters$marker<-paste(unique((position$Feature)))
-      clusters$marker_position<-as.numeric(paste(unique(position$Start)))
-      clusters$difference<-abs(clusters$start-clusters$marker_position)/1000000
-      all_qtls[[length(all_qtls)+1]]<-clusters
-    }else{}
-  } 
+    clusters$qtl<-paste(qtl)
+    clusters$marker<-paste(unique((position$Feature)))
+    clusters$marker_position<-as.numeric(paste(unique(position$Start)))
+    clusters$difference<-abs(clusters$start-clusters$marker_position)/1000000
+    all_qtls[[length(all_qtls)+1]]<-clusters
+  }else{}
+} 
 
 
 qtls_clusters<-do.call(rbind.data.frame, all_qtls)
