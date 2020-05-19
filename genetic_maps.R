673cadea5f1dd0b82e12c8d778407146adad952f
hbenbow <harriet.benbow@gmail.com>
2020-05-19 15:19:04 +0100

    added ssr markers to genetic map script, and updated near2qtls to include ssrs

--- genetic_maps.R
+++ genetic_maps.R
@@ -1,7 +1,8 @@
 setwd("~/Documents/Hotspots/Paper_version_4/Marker_bed/")
+
 chroms<-list()
-for(bed in dir(pattern="iSELECT")){
-  file<-read.delim(bed, header=F, skip=1)
+for(bed in list.files(path="iSelect/")){
+  file<-read.delim(paste("iSelect/", bed, sep=""), header=F, skip=1)
   chromosome=substr(bed, 12, 13)
   file<-file[,1:4]
   colnames(file)<-c("Chromosome", "Start", "End", "Feature")
@@ -11,8 +12,8 @@ for(bed in dir(pattern="iSELECT")){
 iselect<-do.call(rbind.data.frame, chroms)
 
 chroms<-list()
-for(bed in dir(pattern="DArT")){
-  file<-read.delim(bed, header=F, skip=1)
+for(bed in list.files(path="DArT/")){
+  file<-read.delim(paste("DArT/", bed, sep=""), header=F, skip=1)
   chromosome=substr(bed, 14, 15)
   file<-file[,1:4]
   colnames(file)<-c("Chromosome", "Start", "End", "Feature")
@@ -22,7 +23,23 @@ for(bed in dir(pattern="DArT")){
 DarT<-do.call(rbind.data.frame, chroms)
 
 
-markers<-rbind(iselect, DarT)
-write.csv(all_markers_positions, file="~/Documents/Hotspots/Paper_version_4/all_markers_positions.csv")
+chroms<-list()
+for(bed in list.files(path="SSR/gff/", patter="gff3")){
+  file<-read.delim(paste("SSR/gff/", bed, sep=""), header=F, comment.char = "#")
+  names<-as.character(file$V9)
+  split<-do.call('rbind',strsplit(names, split=";"))[,3]
+  split<-do.call('rbind',strsplit(split, split="="))[,2]
+  file2<-as.data.frame(cbind(as.character(file$V1), file$V4, file$V5, split))
+  colnames(file2)<-c("Chromosome", "Start", "End", "Feature")
+  file2$Platform<-"SSR"
+  file3<-file2
+  file3$Feature<-paste("X", tolower(file3$Feature), sep="")
+  file4<-rbind(file2, file3)
+  chroms[[length(chroms)+1]]<-file4
+}
+SSRs<-do.call(rbind.data.frame, chroms)
+
+markers<-rbind(iselect, DarT, SSRs)
+write.csv(markers, file="~/Documents/Hotspots/Paper_version_4/all_markers_positions.csv")
 write.csv(maps, file="~/Documents/Hotspots/Paper_version_4/maps.csv")
 
--- near2qtls.R
+++ near2qtls.R
@@ -5,19 +5,29 @@ Fg_final <- read.csv("~/Documents/Hotspots/Paper_version_4/Fg_final.csv")
 all_markers_positions <- read.csv("~/Documents/Hotspots/Paper_version_4/all_markers_positions.csv", row.names=1)
 final_hotspots <- read.csv("~/Documents/Hotspots/Paper_version_4/final_hotspots.csv")
 QTL_database <- read.csv("~/Documents/Hotspots/Paper_version_4/QTLs/QTL_database.csv")
+QTL_database$Chromosomes<-paste("chr", QTL_database$Chromosomes, sep="")
 
 all_qtls<-list()
 qtls<-QTL_database$General.number
 for(qtl in qtls){
   data<-QTL_database[(QTL_database$General.number==qtl),]
   marker<-as.character(data$Linked.markers)
-  position<-all_markers_positions[(all_markers_positions$Feature=marker),]$Start
-  chromosome<-as.character(all_markers_positions[(all_markers_positions$Feature=marker),]$Chromosome)
-  clusters<-final_hotspots[(final_hotspots$Chromosome==chromosome),]
-  clusters$QTL_position<-position
-  clusters$difference<-abs(clusters$start-clusters$QTL_position)/1000000
-  clusters$qtl<-data$General.number
-  all_qtls[[length(all_qtls)+1]]<-clusters
-  }
+  chr<-data$Chromosomes
+  position<-subset(all_markers_positions, all_markers_positions$Chromosome %in% chr)
+  position<-subset(position, position$Feature %in% marker)
+   clusters<-subset(final_hotspots, final_hotspots$Chromosome %in% chr)
+  len<-nrow(clusters)
+  lenp<-nrow(position)
+  if(lenp>0 && len>0){
+      clusters$qtl<-paste(qtl)
+      clusters$marker<-paste(unique((position$Feature)))
+      clusters$marker_position<-as.numeric(paste(unique(position$Start)))
+      clusters$difference<-abs(clusters$start-clusters$marker_position)/1000000
+      all_qtls[[length(all_qtls)+1]]<-clusters
+    }else{}
+  } 
+
+
 qtls_clusters<-do.call(rbind.data.frame, all_qtls)
-qtls_clusters<-na.omit(qtls_clusters)
+
+
